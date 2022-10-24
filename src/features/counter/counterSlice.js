52b94a91804a2b69409eb36ec27f4575c14a582c
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-10-24 14:30:51 +0100

    Adding store and first reducer

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,28 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    count: 0
-}
+  count: 0,
+};
 
-export const counterSlice = createSlice({
-    name: 'counter',
-    initialState,
-    reducers: {
-        increment: (state) => {
-            state.count += 1;
-        },
-        decrement: (state) => {
-            state.count -= 1;
-        },
-        reset: (state) => {
-            state.count = 0;
-        },
-        incrementByAmount: (state, action) => {
-            state.count += action.payload;
-        }
-    }
+export const counterslice = createSlice({
+  name: "counter",
+  initialState,
+  // create actions with anonymous functions inside this reducers object
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+  },
 });
 
-export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions;
+// destructure actions
+export const { increment, decrement } = counterSlice.actions;
 
 export default counterSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
+// Provider provides global state to the app
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
