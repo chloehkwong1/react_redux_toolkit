6e088bd8f63b301a97e72ed61807df9b45087102
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-10-24 16:09:18 +0100

    Adding react router dom to switch between apps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import Counter from "./features/counter/Counter";
+import Posts from "./features/posts/Posts";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <main className="App">
-      <Counter />
-    </main>
+    <BrowserRouter>
+      <div className="navbar">
+        <ul>
+          <li>
+            <Link to="/counter">Counter</Link>
+          </li>
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="body">
+        <Routes>
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/posts" element={<Posts />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
