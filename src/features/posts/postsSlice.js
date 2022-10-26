3ae163c80a60911b49aa6b0501791f7c9a934f9c
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-10-26 17:20:05 +0100

    Changing post state structure

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,34 +1,11 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
 
-const initialState = [
-  {
-    id: "1",
-    title: "Learning Redux Toolkit",
-    content: "I've heard good things.",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-  {
-    id: "2",
-    title: "Slices...",
-    content: "The more I say slice, the more I want pizza.",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-];
+const initialState = {
+  posts: [],
+  status: "idle", //'idle' | 'loading' | 'succeeded' | 'failed'
+  error: null,
+};
 
 const postsSlice = createSlice({
   name: "posts",
@@ -36,7 +13,7 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       // prepare callback function that is used to construct the payload value
       prepare(title, content, userId) {
@@ -60,7 +37,7 @@ const postsSlice = createSlice({
     },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
-      const existingPost = state.find((post) => post.id === postId);
+      const existingPost = state.posts.find((post) => post.id === postId);
       if (existingPost) {
         //immer js makes sure we don't mutate the state
         existingPost.reactions[reaction]++;
@@ -70,7 +47,7 @@ const postsSlice = createSlice({
 });
 
 // define this here instead of at component level in case the structure of state changes. this way we only need to change here
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
 
 export const { postAdded, reactionAdded } = postsSlice.actions;
 
