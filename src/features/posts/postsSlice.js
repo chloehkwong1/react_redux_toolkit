3f97ebbd43e2a5e9bacc9114f881624b403f6944
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-10-26 12:37:48 +0100

    Adding TimeAgo component

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -31,7 +31,7 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
-            date: new Date().toISOString,
+            date: new Date().toISOString(),
             userId,
           },
         };
