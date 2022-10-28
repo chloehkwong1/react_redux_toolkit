cc3b9afa4c60cd2caab22769a6812fdd095ce326
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-10-28 18:03:18 +0100

    Updated form to add post async

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import { postAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
@@ -11,6 +11,7 @@ const AddPostForm = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [userId, setUserId] = useState("");
+  const [addRequestStatus, setAddRequestStatus] = useState("idle");
 
   const users = useSelector(selectAllUsers);
 
@@ -18,25 +19,35 @@ const AddPostForm = () => {
   const onContentChanged = (e) => setContent(e.target.value);
   const onAuthorChanged = (e) => setUserId(e.target.value);
 
-  const navigate = useNavigate();
-  const onSavePostClicked = () => {
-    // we want to abstract this logic to the reducer in case the state format changes
-    if (title && content) {
-      dispatch(postAdded(title, content, userId));
-
-      setTitle("");
-      setContent("");
-      navigate("/posts-list");
-    }
-  };
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === "idle";
 
   const usersOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
       {user.name}
     </option>
   ));
+
+  const onSavePostClicked = () => {
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        // The promise returned by the dispatched thunk has an unwrap property
+        // which can be called to extract the payload of a fulfilled action or
+        // to throw either the error or, if available, payload created by
+        // rejectWithValue from a rejected action
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
+
+        setTitle("");
+        setContent("");
+        setUserId("");
+      } catch (err) {
+        console.log("Failed to save the post", err);
+      } finally {
+        setAddRequestStatus("idle");
+      }
+    }
+  };
 
   return (
     <section>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -14,6 +14,13 @@ export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
   const response = await axios.get(POSTS_URL);
   return response.data;
 });
+export const addNewPost = createAsyncThunk(
+  "posts/addNewPost",
+  async (initialPost) => {
+    const response = await axios.post(POSTS_URL, initialPost);
+    return response.data;
+  }
+);
 
 const postsSlice = createSlice({
   name: "posts",
@@ -78,6 +85,18 @@ const postsSlice = createSlice({
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        action.payload.userId = Number(action.payload.userId);
+        action.payload.date = new Date().toISOString();
+        action.payload.reactions = {
+          thumbsUp: 0,
+          wow: 0,
+          heart: 0,
+          rocket: 0,
+          coffee: 0,
+        };
+        state.posts.push(action.payload);
       });
   },
 });
