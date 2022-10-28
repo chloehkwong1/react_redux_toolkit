15ca1516d906ba6bd4431a1bc607d8a95f1aa513
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-10-28 14:47:03 +0100

    Finished linking up async thunk and refactoring

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -7,9 +7,7 @@ import {
 } from "./postsSlice";
 import { useEffect } from "react";
 
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
   const dispatch = useDispatch();
@@ -24,25 +22,24 @@ const PostsList = () => {
     }
   }, [postStatus, dispatch]);
 
-  const reversePosts = posts.slice(0).reverse();
-  //or const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date))
-
-  const renderedPosts = reversePosts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
-      <p>
-        <PostAuthor className="postCredit" userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-      <ReactionButtons post={post} />
-    </article>
-  ));
+  let content;
+  if (postStatus === "loading") {
+    content = <p>"Loading...</p>;
+  } else if (postStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post} />
+    ));
+  } else if (postStatus === "failed") {
+    content = <p>{error}</p>;
+  }
 
   return (
     <section>
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 };
